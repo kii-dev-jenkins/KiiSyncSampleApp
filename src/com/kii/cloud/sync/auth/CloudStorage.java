01c09b7d4fd6162b228a45e4b739f6b6fa3bcb80
hueymeng.tan <hueymeng.tan@e7cb0379-d053-0e4b-a1ac-43c7dbffadbe>
2012-02-16 07:10:52 +0000

    HM: Refactor the classname for the manager HM: rename some of the classes name and change KiiObject to KiiFlex to make it consistent with Flex Storage concept.
    git-svn-id: https://source.kii.com/servo/platform/sdk-mobile-client/android/KiiSyncSampleApp@20128 e7cb0379-d053-0e4b-a1ac-43c7dbffadbe

--- src/com/kii/cloud/sync/auth/CloudStorage.java
+++ src/com/kii/cloud/sync/auth/CloudStorage.java
@@ -1,17 +1,19 @@
 package com.kii.cloud.sync.auth;
 
+import java.io.IOException;
+
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 
 import com.kii.cloud.storage.EasyClient;
-import com.kii.cloud.storage.UserResult;
 import com.kii.cloud.storage.dataType.KiiUser;
-import com.kii.cloud.storage.manager.KiiUserManager;
+import com.kii.cloud.storage.manager.AuthManager;
+import com.kii.cloud.storage.response.CloudExecutionException;
+import com.kii.cloud.storage.response.UserResult;
 import com.kii.cloud.sync.Authentication;
 import com.kii.sync.KiiClient;
 import com.kii.sync.KiiUMInfo;
@@ -31,7 +33,7 @@ public class CloudStorage implements Authentication{
     static final String PROPERTY_COUNTRY = "country";
     	
 	KiiClient mSyncClient = null;
-	KiiUserManager mUserMgr = null;
+	AuthManager mUserMgr = null;
 	Context mContext;
 
 	public CloudStorage(Context context, KiiClient client){
@@ -69,9 +71,24 @@ public class CloudStorage implements Authentication{
 		
 		if(oldPassword.compareTo(password)==0){
 			if( mUserMgr.getLoginUser() == null ){
-				mUserMgr.login(username, oldPassword);
+				
+				try {
+					mUserMgr.login(username, oldPassword);
+				} catch (CloudExecutionException e) {
+					return SyncMsg.ERROR_AUTHENTICAION_ERROR;
+				} catch (IOException e) {
+					return SyncMsg.ERROR_IO;
+				}
 			}
-			UserResult result = mUserMgr.changePassword(oldPassword, newPassword);
+			UserResult result=null;
+			try {
+				result = mUserMgr.changePassword(oldPassword, newPassword);
+			} catch (CloudExecutionException e) {
+				return SyncMsg.ERROR_SERVER_TEMP_ERROR;
+			} catch (IOException e) {
+				return SyncMsg.ERROR_IO;
+			}
+
 			if( result.getKiiUser() != null ){
 				SyncPref.setPassword(newPassword);
 				return SyncMsg.OK;
@@ -88,6 +105,11 @@ public class CloudStorage implements Authentication{
 		}
 		KiiUser user = new KiiUser();
 		user.setUsername(userName);
+		
+		if(userName.contains("@")){
+			user.setEmail(userName);
+		}
+		
 		if(!TextUtils.isEmpty(country)){
 			user.setStringProperty(PROPERTY_COUNTRY, country);
 		}
@@ -97,12 +119,16 @@ public class CloudStorage implements Authentication{
 		if(!TextUtils.isEmpty(mobile)){
 			user.setName(mobile);
 		}
-		UserResult result = mUserMgr.createUser(user, password);
-		user = result.getKiiUser();
-		if(user==null){
-			Log.e(TAG, result.getException().getMessage());
-			return SyncMsg.ERROR_SERVER_TEMP_ERROR;
+		UserResult result;
+		try {
+			result = mUserMgr.createUser(user, password);
+		} catch (CloudExecutionException e) {
+			return SyncMsg.ERROR_INVALID_INPUT;
+		} catch (IOException e) {
+			return SyncMsg.ERROR_IO;
 		}
+		user = result.getKiiUser();
+		
 		if( userName.compareTo(user.getUsername())==0)
 			return SyncMsg.OK;
 		
@@ -111,16 +137,27 @@ public class CloudStorage implements Authentication{
 
 	@Override
 	public int login(String username, String password) {
-		UserResult result = mUserMgr.login(username, password);
+		UserResult result;
+		try {
+			result = mUserMgr.login(username, password);
+		} catch (CloudExecutionException e) {
+			return SyncMsg.ERROR_AUTHENTICAION_ERROR;
+		} catch (IOException e) {
+			return SyncMsg.ERROR_IO;
+		}
+		String accType = "KII_ID";
 		KiiUser user = result.getKiiUser();
 		if(user!=null){
 			SyncPref.setUsername(username);
+			if(username.contains("@")){
+				accType = "EMAIL";
+			}
 			KiiUMInfo info = new KiiUMInfo(mContext,
 					username, 
 					password,  
 					"http://dev-usergrid.kii.com/app/sync/pfs",
-					"KII_ID",
-					null);
+					accType,
+					username);
 			mSyncClient.setKiiUMInfo(info);
 		}
 		return 0;
@@ -130,7 +167,8 @@ public class CloudStorage implements Authentication{
 
 	@Override
 	public int logout() {
-		// TODO Auto-generated method stub
+		// clear the session
+		mUserMgr.logout();
 		return 0;
 	}
 	
