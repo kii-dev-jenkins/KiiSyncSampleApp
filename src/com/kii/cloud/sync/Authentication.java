dd30a06d1c5f9001bae80d321c105ca9948f3fbd
yang.tian <yang.tian@e7cb0379-d053-0e4b-a1ac-43c7dbffadbe>
2012-03-19 03:45:09 +0000

    TianYang: format code
    git-svn-id: https://source.kii.com/servo/platform/sdk-mobile-client/android/KiiSyncSampleApp@21330 e7cb0379-d053-0e4b-a1ac-43c7dbffadbe

--- src/com/kii/cloud/sync/Authentication.java
+++ src/com/kii/cloud/sync/Authentication.java
@@ -9,7 +9,7 @@ public interface Authentication {
      * @param newPassword
      */
     public int changePassword(String oldPassword, String newPassword);
-    
+
     /**
      * Register user if no account has been registered
      * 
@@ -24,26 +24,25 @@ public interface Authentication {
      * @param mobile
      *            :
      * @return
-     *
      */
     public int register(String userName, String password, String country,
             String nickName, String mobile);
-    
+
     /**
-     * Log in using the given username and password. 
-     * If the given password is different from previous one, it will login again
-     * else just return OK.
+     * Log in using the given username and password. If the given password is
+     * different from previous one, it will login again else just return OK.
      * 
      * @param username
      * @param password
      * @return
      */
     public int login(String username, String password);
-    
+
     /**
      * Logout by clearing all the user information.
+     * 
      * @return
      */
     public int logout();
-    
+
 }
